a728ec6a9d3e82a5cba501f43d4eccb9a76999e4
annkonna <annkonna@buffalo.edu>
2019-10-25 17:04:20 -0400

    Initial project version

---
+++ tabify.c
@@ -0,0 +1,11 @@
+/* tabify: Read standard input, converting spaces to tabs and writing
+ *           to standard output. */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    /* Your implementation should appear here */
+
+    return 0;
+}
