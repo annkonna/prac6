98a5b8512eaf3dcc1e6d22c01ecb7bb6058f0b33
annkonna <annkonna@buffalo.edu>
2019-10-27 14:53:48 -0400

    modified tabify function

--- tabify.c
+++ tabify.c
@@ -6,6 +6,27 @@
 int main(int argc, char *argv[])
 {
     /* Your implementation should appear here */
-
+    char input = getchar();
+    int count = 0;
+    while(input != EOF){
+        if(input == ' '){
+            count = count + 1;
+            input = getchar();
+        }
+        else{
+            if(count == 1){
+                putchar(' ');
+                putchar(input);
+            }
+            else{
+                for(int i = 0; i < ((count%8 == 0) ? count/8 : 1 + count/8); i++){
+                    putchar('\t');
+                }
+                putchar(input);
+            }
+            count = 0;
+            input = getchar();
+        }
+    }
     return 0;
 }
